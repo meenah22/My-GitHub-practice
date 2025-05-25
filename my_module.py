57b385fb27cd8c807bbbe4089ee40d98ee7fc78e
meenah22 <ameenatola123@gmail.com>
2025-05-25 13:10:34 +0100

    Create my_module.py

---
+++ my_module.py
@@ -0,0 +1,12 @@
+def add(x, y):
+  """Return the sum of two numbers."""
+  return x + y
+
+def multiply(x, y):
+  """Return the product of two numbers."""
+  return x * y
+
+if __name__ == "__main__":
+  result1 = add(3, 5)
+  result2 = multiply(4, 6)
+
